cb09c392479d5ea401a4dbda99def8847bd12ddb
Rahulalvk <rahulal432@gmail.com>
2023-05-03 22:18:17 -0700

    Programe to reverse a number

---
+++ ReverseNumber/ReverseAnumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    internal class ReverseAnumber
+    {
+        public static void DisplayReverseOfaNumber() 
+        {
+            int reverse =0, rem=0;
+            Console.WriteLine("Enter the number :");
+            int n =Convert.ToInt32(Console.ReadLine());
+            while (n !=0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n=n / 10;
+            }
+            Console.WriteLine(reverse);
+        }
+    }
+}
